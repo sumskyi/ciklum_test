f003c0aa3318fb7112d25fd9231901b7bd041390
vlad <sumskyi@gmail.com>
2011-11-14 15:26:11 +0200

    scary solution added

--- reevoo_checkout.rb
+++ reevoo_checkout.rb
@@ -2,23 +2,80 @@
 
 module Reevoo
   class Checkout
+    attr_reader :items
+
+    def initialize(pricing_rules)
+      @items          = []
+      @pricing_rules  = pricing_rules
+    end
+
     def scan(item)
+      @items << item
     end
 
     def total
+      @pricing_rules.items = @items.dup
+      items.inject(0.0){|acc, item| acc += item.price } - @pricing_rules.total_discount
     end
   end
 
   class PricingRules
-  end
+    attr_accessor :items
 
-  class Item
-    def initialize(name)
+    def initialize
+      @rules = [:fruit_tea, :strawberries]
+      @total_discount = 0.0
+    end
+
+    def total_discount
+      @rules.each{|rule| self.send("apply_#{rule}_discount") }
+      @total_discount
+    end
+
+  private
+    def apply_fruit_tea_discount
+      fruit_tea_count = items.count{|el| :fr1 == el.name }
+      @total_discount += fruit_tea_count / 2 * fruit_tea_price
+    end
+
+    def apply_strawberries_discount
+      strawberries_count = items.count{|el| :sr1 == el.name }
+      @total_discount += strawberries_count * 0.50 if strawberries_count > 2
+    end
+
+    def fruit_tea_price
+      sample_fruit = items.first{|el| :fr1 == el.name }
+      sample_fruit.price or 0.0
     end
   end
 
-  def self.Item(name)
-    Item.new(name)
+  class Item
+    Detail = Struct.new(:name, :price, :description)
+    @@items = {
+      fr1: Detail.new(:fr1, 3.11,   'fruit tea'),
+      sr1: Detail.new(:sr1, 5.00,   'Strawberries'),
+      cf1: Detail.new(:cf1, 11.23,  'Coffee')
+    }
+
+    class NotFound < StandardError; end
+
+    attr_reader :name, :description
+    attr_accessor :price
+
+    class << self
+      def find(id)
+        @@items[id.to_sym]
+      end
+    end
+
+    def initialize(id)
+      item = self.class.find(id) or raise NotFound
+      @name, @price, @description = item.name, item.price, item.description
+    end
+  end
+
+  def self.Item(id)
+    Item.new(id)
   end
 end
 
